7efd4f5e908200782ece7573af686f73bd3abfe5
you@christopherdominic <chriseze0@gmail.com>
2025-10-18 00:17:40 +0100

    nddngdjfj

--- task_4.sql
+++ task_4.sql
@@ -2,4 +2,19 @@
 USE alx_book_store;
 
 -- Display the full description of the table 'books' without using  or EXPLAIN
-SHOW CREATE TABLE Books;
+-- Print full description of the 'Books' table using INFORMATION_SCHEMA
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY
+    ORDINAL_POSITION;
+
